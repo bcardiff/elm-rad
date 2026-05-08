e64bc75dddd8db5d0889910f0def81df7a5de4e1
Brian J. Cardiff <bcardiff@gmail.com>
2026-05-08 20:20:10 -0300

    Add PersistSaveDebounceTest placeholder

---
+++ tests/PersistSaveDebounceTest.elm
@@ -0,0 +1,13 @@
+module PersistSaveDebounceTest exposing (suite)
+
+import Expect
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Persist save debounce"
+        [ test "placeholder for runtime-level integration tests" <|
+            \_ ->
+                Expect.pass
+        ]
